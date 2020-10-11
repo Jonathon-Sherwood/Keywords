a033405bbbef27c414f63b4ebcd8a3176c857a61
Jonathon Sherwood <jonathondsherwood@hotmail.com>
2020-10-11 15:33:57 -0700

    Project Shell

---
+++ Keywords/Keywords.cpp
@@ -0,0 +1,25 @@
+/*
+	Jonathon Sherwood
+	10/11/2020
+	Keywords: Practice in Arrays and Strings
+*/
+
+
+#include <iostream> //Used to display messages to the console
+
+#include <string> //Allows for strings to be used as variables
+
+
+using std::cout; //Global declaration to shorten the cout command namespace throughout the code
+
+using std::cin; //Global declaration to shorten the cin command namespace throughout the code
+
+using std::endl; //Global declaration to shorten the endl command namespace throughout the code
+
+using std::string; //Global declaration to shorten the string command namespace throughout the code
+
+
+int main() //Starting function containig our code found in every C++ file 
+{
+	
+}
